e69d01b2f4fe3330ec8e89572e4c9b44b77f9c30
Özgür Türkoğlu <138875953+ozgurtrko@users.noreply.github.com>
2024-09-19 15:09:58 +0300

    Update nba-api.py

--- nba-api.py
+++ nba-api.py
@@ -344,7 +344,7 @@ def main():
     with open('style.css') as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
     
-    st.title("NBA Player and Team Statistics")
+    st.title("NBA Data Viewer")
 
     option = st.selectbox("Select an option:", ["League Leaders", "Team", "Player"])
 
